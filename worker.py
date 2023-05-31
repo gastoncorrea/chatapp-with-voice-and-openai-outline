dfeb660bb1b44957ef00d34cbd6de47b4d4213c8
Gaston <gastoncorrea90e@gmail.com>
2023-05-31 12:03:12 -0300

    Add function to request Watson Api and transcript to voice request as text

--- worker.py
+++ worker.py
@@ -1,11 +1,30 @@
 import openai
 import requests
 
-openai.api_key = "..."
+openai.api_key = "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"
 
 
 def speech_to_text(audio_binary):
-    return None
+    #Set up Watson Speech to Text HTTP Api url
+    base_url = 'https://sn-watson-stt.labs.skills.network'
+    api_url = base_url+'/speech-to-text/api/v1/recognize'
+    #Set up parameters for our HTTP request
+    params = {
+        'model':'es-LA_Multimedia',
+    }
+    #Set up the body of our HTTP request
+    body = audio_binary
+
+    #Send a HTTP Post request
+    response = requests.post(api_url, params=params, data=body).json()
+
+    #parse the response to get our transcribed text
+    text = 'null'
+    while bool(response.get('results')):
+        print('speech to text response', response)
+        text = response.get('results').pop().get('alternative').pop().get('transcript')
+        print('recognised text: ', text)
+    return text
 
 
 def text_to_speech(text, voice=""):
