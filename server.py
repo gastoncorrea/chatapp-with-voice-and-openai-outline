30d3172e0d8e6d2dcff41de61a91f478d6c1ce25
Gaston <gastoncorrea90e@gmail.com>
2023-06-01 16:18:22 -0300

    add request routes to response front-end

--- server.py
+++ server.py
@@ -16,16 +16,46 @@ def index():
 
 @app.route('/speech-to-text', methods=['POST'])
 def speech_to_text_route():
-    return None
+    print('processing speech to text')
+    audio_binary = request.data #Get the user's speech from their request
+    text = speech_to_text(audio_binary) #call speech_to_text function to transcribe the speech 
+   #Return the response back to user in JSON format 
+    response = app.response_class(
+       response = json.dumps({'text': text}),
+       status = 200,
+       mimeType = 'application/json')
+    print(response)
+    print(response.data)
+    return response
 
 
 @app.route('/process-message', methods=['POST'])
 def process_prompt_route():
+    user_message = request.json['user_message']
+    print('user Message: ', user_message)
+
+    voice = request.json['voice']
+    print('voice: ', voice)
+
+    openai_response_text = openai_process_message(user_message)
+    
+    #clean the response to remove any emptylines
+    openai_response_text = os.linesep.join([s for s in openai_response_text.splitlines() if s])
+
+    # Call our text_to_speech function to convert OpenAI Api's reponse to speech
+    openai_response_speech = text_to_speech(openai_response_text, voice)
+
+    # convert openai_response_speech to base64 string so it can be sent back in the JSON response
+    openai_response_speech = base64.b64encode(openai_response_speech).decode('utf-8')
+
+    # Send a JSON response back to the user containing their message's response both in text and speech format
     response = app.response_class(
-        response=json.dumps({"openaiResponseText": None, "openaiResponseSpeech": None}),
+        response=json.dumps({"openaiResponseText": openai_response_text, "openaiResponseSpeech": openai_response_speech}),
         status=200,
         mimetype='application/json'
     )
+
+    # Send a JSON response back to the user containing their message's response both in text and speech formats
     return response
 
 
